c7a31e8899b69d2c28301d755c3b1de914121314
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-22 19:24:58 +0500

    f: Frontend Auth Implement

---
+++ frontend/js/login.js
@@ -0,0 +1,40 @@
+
+
+
+document.addEventListener("submit", async function(e){
+
+
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    e.preventDefault();
+
+    const payload = {
+        email: email.value.trim(),
+        password: password.value.trim(),
+    };
+
+
+    if (!payload.email || !payload.password) {
+        return alert("All fields are required.");
+    }
+
+    try {
+        const { data } = await axios.post(
+            "http://localhost:8000/api/auth/login",
+            payload
+        );
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem(
+            "user",
+            JSON.stringify(data.user)
+        );
+
+        window.location.href = "/frontend/pages/dashboard.html";
+
+    } catch (error) {
+        console.log(error.response.data)
+    }
+})
+
