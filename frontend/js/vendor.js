b7281774ce5ad406401e9f75a39686f1dbd6cb0a
abdul Rafay <abdulrafay700000@gmail.com>
2026-06-26 19:17:13 +0500

    f:(vendors) Frontend index Api call

--- frontend/js/vendor.js
+++ frontend/js/vendor.js
@@ -6,11 +6,32 @@ document
     });
 
 
+    if(localStorage.getItem("token")){
+        index()
+    }
+
+
+   async function index(){
+    
 
     try {
 
-        const {data} = axios.get("")
+        const {data} = await axios.get("http://127.0.0.1:8000/api/auth/vendors",
+            {
+                headers:{
+                    'Authorization': `Bearer ${localStorage.getItem("token")}`,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+
+        console.log(data)
         
     } catch (error) {
-        
+        console.log(error)
     }
+
+    }
+
+    
